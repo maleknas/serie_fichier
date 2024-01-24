143829f6995b5a71b966773399ade3799d575d0a
malek nasraoui <148629570+maleknas@users.noreply.github.com>
2024-01-24 23:10:32 +0100

    Update ex_n°2.c

--- ex_n°2.c
+++ ex_n°2.c
@@ -1,17 +1,21 @@
 //ex n°2 : affiche fichier :
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 void main(){
     //initialisation du nom de fichier :
     char noun[100];
     printf("donner le nom du fichier 'file.txt' : ");
     gets(noun);
+    printf("le noun du fichier est : %s\n",noun);
+    strcat(noun,".txt");
+    printf("le noun du fichier  est (apres mod): %s\n",noun);
     //affichage du contenue du fichier :
     FILE* file=fopen(noun,"r");
     if(file!=NULL){
         char ch[100];
         if(fgets(ch,100,file)!=NULL){
-            printf("%S",ch);
+            printf("%s",ch);
         }
         fclose(file);
     }else{printf("error!!!");}
