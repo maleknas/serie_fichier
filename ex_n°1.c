1e419c37f378c77d521ef316234f043a64c61ada
malek nasraoui <148629570+maleknas@users.noreply.github.com>
2024-01-24 19:44:09 +0100

    Create ex_n°1.c

---
+++ ex_n°1.c
@@ -0,0 +1,13 @@
+//ex n°1 : affiche fichier :
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    FILE* file=fopen("file.txt","r");
+    if(file!=NULL){
+        char ch[100];
+        if(fgets(ch,100,file)!=NULL){
+            printf("%S",ch);
+        }
+        fclose(file);
+    }else{printf("error!!!");}
+}
